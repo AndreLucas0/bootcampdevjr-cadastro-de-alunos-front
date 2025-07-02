d4c13024374b277ceda597a89fbd21146b578d91
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-07-02 10:40:34 -0300

    load students - closes #3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
 $(document).ready(function(){
     $('#inputPhone  ').mask('(00) 00000-0000');
   });
+
+  var students = [
+    {
+      id: 1,
+      name: "André",
+      email: "andre@gmail.com",
+      phone: "11999999999",
+      course: 1,
+      shift: 1
+    },
+    {
+      id: 2,
+      name: "Lucas",
+      email: "lucas@gmail.com",
+      phone: "11999999999",
+      course: 2,
+      shift: 2
+    },
+    {
+      id: 3,
+      name: "Ferreira",
+      email: "ferreira@gmail.com",
+      phone: "11999999999",
+      course: 3,
+      shift: 3
+    },
+  ];
+
+  var shifts = [
+    { id: 1, name: "Java"},
+    { id: 2, name: "Angular"},
+    { id: 3, name: "Spring"}
+  ];
+
+  loadStudents();
+
+  function loadStudents() {
+    for (let stud of students) {
+      addNewRow(stud);
+    }
+  }
+
+  
